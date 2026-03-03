7e7392f9ee0aa3bf0a83af457bc1e8fde4277688
Dan Kingo <kingotchs23@gmail.com>
2026-03-03 22:22:43 +1200

    inital commit

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1 class="text-xl font-bold p-4">Hello.</h1>
+    <Button class="cursor-pointer" @click="() => console.log('Button clicked!')">Click Me</Button>
+  </div>
+</template>
